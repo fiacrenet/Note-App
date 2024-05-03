a1051cd27dd0c9a9faaac89fad4861e700ab9248
Fiacre <fiacreyatta@gmail.com>
2024-05-04 00:28:48 +0200

    last update

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
-const apiUrl = "/choreo-apis/noteapp/backend/rest-api-be2/v1"
+const apiUrl = "/choreo-apis/noteapp/backend/rest-api-be2/v1";
 
 const api = axios.create({
 
-    baseURL:import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL:apiUrl,
+    baseURL:import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL: apiUrl,
 
 });
 
